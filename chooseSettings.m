d837f1cfd5773db7855025c10bc14e76e4e64606
Mereghetti Alessio <Alessio.Mereghetti@Main.Cnao.group>
2023-02-27 15:02:57 +0100

    Adding configuration files and crunching of measurements of mistery of XPR CAMeretta

--- chooseSettings.m
+++ chooseSettings.m
@@ -6,6 +6,259 @@
 switch upper(loadSettings)
 
     % =====================================================================
+	% %%%%%%%%%%%%%%%%% MISTERY of INTEGRAL %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % compare measurements 2022-10-08 (amereghe's, with problem on
+    % integral) vs 2023-02-26 (donetti's, without problem on integral)
+    % CAMeretta ONLT, to compare data sets
+    % =====================================================================
+    case "XPR3,C270,HE-025A-QUE,SOLUTION"
+        myTit="XPR3, C270mm, HE-025A-QUE scan (2022-10-08 vs 2023-02-26) - Role of HV";
+        monTypes=["CAM" "CAM"];
+        MonPaths=[...
+            "P:\Accelerating-System\Accelerator-data\Area dati MD\00XPR\XPR3\carbonio\Fuochi\Rough_scan\C_270mm\HE-025A-QUE\CarbSO2_*\"
+            "S:\Area Ricerca\XPR\data\CAMeretta\2023-02-26_ProblemaIntegrale\Carbonio\HE-025A-QUE\270mm\Run7CamNoHV\CarbSO2_*\"
+            ];
+        myLabels=[
+            "ref (2022-10-08)"
+            "CAM XPR (2023-02-26) - NO HV"
+            ];
+        shifts=[0 2];
+
+    case "XPR3,C270,HE-025A-QUE,SOLUTION(ALL)"
+        myTit="XPR3, C270mm, HE-025A-QUE scan (2022-10-08 vs 2023-02-26) - CAMeretta only";
+        monTypes=["CAM" "CAM" "CAM" "CAM" "CAM" "CAM" "CAM" "CAM"];
+        MonPaths=[...
+            "P:\Accelerating-System\Accelerator-data\Area dati MD\00XPR\XPR3\carbonio\Fuochi\Rough_scan\C_270mm\HE-025A-QUE\CarbSO2_*\"
+            "S:\Area Ricerca\XPR\data\CAMeretta\2023-02-26_ProblemaIntegrale\Carbonio\HE-025A-QUE\270mm\Run1CamXPR\CarbSO2_*\"
+            "S:\Area Ricerca\XPR\data\CAMeretta\2023-02-26_ProblemaIntegrale\Carbonio\HE-025A-QUE\270mm\Run2CamXPR\CarbSO2_*\"
+            "S:\Area Ricerca\XPR\data\CAMeretta\2023-02-26_ProblemaIntegrale\Carbonio\HE-025A-QUE\270mm\Run3CamOldRev\CarbSO2_*\"
+            "S:\Area Ricerca\XPR\data\CAMeretta\2023-02-26_ProblemaIntegrale\Carbonio\HE-025A-QUE\270mm\Run4CamOldRev\CarbSO2_*\"
+            "S:\Area Ricerca\XPR\data\CAMeretta\2023-02-26_ProblemaIntegrale\Carbonio\HE-025A-QUE\270mm\Run5CamOld\CarbSO2_*\"
+            "S:\Area Ricerca\XPR\data\CAMeretta\2023-02-26_ProblemaIntegrale\Carbonio\HE-025A-QUE\270mm\Run6CamOld\CarbSO2_*\"
+            "S:\Area Ricerca\XPR\data\CAMeretta\2023-02-26_ProblemaIntegrale\Carbonio\HE-025A-QUE\270mm\Run7CamNoHV\CarbSO2_*\"
+            ];
+        myLabels=[
+            "ref (2022-10-08)"
+            "CAM XPR (2023-02-26) - 1"
+            "CAM XPR (2023-02-26) - 2"
+            "CAM OP rotated (2023-02-26) - 1"
+            "CAM OP rotated (2023-02-26) - 2"
+            "CAM OP (2023-02-26) - 1"
+            "CAM OP (2023-02-26) - 2"
+            "CAM XPR (2023-02-26) - NO HV"
+            ];
+        shifts=[0 2 2 2 2 2 2 2 2];
+
+    % =====================================================================
+	% %%%%%%%%%%%%%%%%% MISTERY of INTEGRAL %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % measurements 2022-10-08 (amereghe's, with problem on integral)
+    % CAMeretta vs DDS
+    % =====================================================================
+
+    % ---------------------------------------------------------------------
+    % C270mm
+    % ---------------------------------------------------------------------
+    case "XPR3,C270,HE-025A-QUE(2022-10-08)"
+        myTit="XPR3, C270mm, HE-025A-QUE scan (2022-10-08)";
+        monTypes=["CAM" "DDS"];
+        MonPaths=[...
+            "P:\Accelerating-System\Accelerator-data\Area dati MD\00XPR\XPR3\carbonio\Fuochi\Rough_scan\C_270mm\HE-025A-QUE\CarbSO2_*\"
+            "P:\Accelerating-System\Accelerator-data\Area dati MD\00XPR\XPR3\carbonio\Fuochi\Rough_scan\C_270mm\HE-025A-QUE\PRC-544-*\"
+            ];
+        myLabels=monTypes;
+        shifts=[1 0];
+    case "XPR3,C270,HE-023A-QUE(2022-10-08)"
+        myTit="XPR3, C270mm, HE-023A-QUE scan (2022-10-08)";
+        monTypes=["CAM" "DDS"];
+        MonPaths=[...
+            "P:\Accelerating-System\Accelerator-data\Area dati MD\00XPR\XPR3\carbonio\Fuochi\Rough_scan\C_270mm\HE-023A-QUE\CarbSO2_*\"
+            "P:\Accelerating-System\Accelerator-data\Area dati MD\00XPR\XPR3\carbonio\Fuochi\Rough_scan\C_270mm\HE-023A-QUE\PRC-544-*\"
+            ];
+        myLabels=monTypes;
+        shifts=[1 0];
+    case "XPR3,C270,HE-020A-QUE(2022-10-08)"
+        myTit="XPR3, C270mm, HE-020A-QUE scan (2022-10-08)";
+        monTypes=["CAM" "DDS"];
+        MonPaths=[...
+            "P:\Accelerating-System\Accelerator-data\Area dati MD\00XPR\XPR3\carbonio\Fuochi\Rough_scan\C_270mm\HE-020A-QUE\CarbSO2_*\"
+            "P:\Accelerating-System\Accelerator-data\Area dati MD\00XPR\XPR3\carbonio\Fuochi\Rough_scan\C_270mm\HE-020A-QUE\PRC-544-*\"
+            ];
+        myLabels=monTypes;
+        shifts=[1 0];
+    case "XPR3,C270,HE-018A-QUE(2022-10-08)"
+        myTit="XPR3, C270mm, HE-018A-QUE scan (2022-10-08)";
+        monTypes=["CAM" "DDS"];
+        MonPaths=[...
+            "P:\Accelerating-System\Accelerator-data\Area dati MD\00XPR\XPR3\carbonio\Fuochi\Rough_scan\C_270mm\HE-018A-QUE\CarbSO2_*\"
+            "P:\Accelerating-System\Accelerator-data\Area dati MD\00XPR\XPR3\carbonio\Fuochi\Rough_scan\C_270mm\HE-018A-QUE\PRC-544-*\"
+            ];
+        myLabels=monTypes;
+        shifts=[1 0];
+
+    % ---------------------------------------------------------------------
+    % C150mm
+    % ---------------------------------------------------------------------
+    case "XPR3,C150,HE-025A-QUE(2022-10-08)"
+        myTit="XPR3, C150mm, HE-025A-QUE scan (2022-10-08)";
+        monTypes=["CAM" "DDS"];
+        MonPaths=[...
+            "P:\Accelerating-System\Accelerator-data\Area dati MD\00XPR\XPR3\carbonio\Fuochi\Rough_scan\C_150mm\HE-025A-QUE\CarbSO2_*\"
+            "P:\Accelerating-System\Accelerator-data\Area dati MD\00XPR\XPR3\carbonio\Fuochi\Rough_scan\C_150mm\HE-025A-QUE\PRC-544-*\"
+            ];
+        myLabels=monTypes;
+        shifts=[1 0];
+    case "XPR3,C150,HE-023A-QUE(2022-10-08)"
+        myTit="XPR3, C150mm, HE-023A-QUE scan (2022-10-08)";
+        monTypes=["CAM" "DDS"];
+        MonPaths=[...
+            "P:\Accelerating-System\Accelerator-data\Area dati MD\00XPR\XPR3\carbonio\Fuochi\Rough_scan\C_150mm\HE-023A-QUE\CarbSO2_*\"
+            "P:\Accelerating-System\Accelerator-data\Area dati MD\00XPR\XPR3\carbonio\Fuochi\Rough_scan\C_150mm\HE-023A-QUE\PRC-544-*\"
+            ];
+        myLabels=monTypes;
+        shifts=[1 0];
+    case "XPR3,C150,HE-020A-QUE(2022-10-08)"
+        myTit="XPR3, C150mm, HE-020A-QUE scan (2022-10-08)";
+        monTypes=["CAM" "DDS"];
+        MonPaths=[...
+            "P:\Accelerating-System\Accelerator-data\Area dati MD\00XPR\XPR3\carbonio\Fuochi\Rough_scan\C_150mm\HE-020A-QUE\CarbSO2_*\"
+            "P:\Accelerating-System\Accelerator-data\Area dati MD\00XPR\XPR3\carbonio\Fuochi\Rough_scan\C_150mm\HE-020A-QUE\PRC-544-*\"
+            ];
+        myLabels=monTypes;
+        shifts=[1 0];
+    case "XPR3,C150,HE-018A-QUE(2022-10-08)"
+        myTit="XPR3, C150mm, HE-018A-QUE scan (2022-10-08)";
+        monTypes=["CAM" "DDS"];
+        MonPaths=[...
+            "P:\Accelerating-System\Accelerator-data\Area dati MD\00XPR\XPR3\carbonio\Fuochi\Rough_scan\C_150mm\HE-018A-QUE\CarbSO2_*\"
+            "P:\Accelerating-System\Accelerator-data\Area dati MD\00XPR\XPR3\carbonio\Fuochi\Rough_scan\C_150mm\HE-018A-QUE\PRC-544-*\"
+            ];
+        myLabels=monTypes;
+        shifts=[1 0];
+
+    % ---------------------------------------------------------------------
+    % C030mm
+    % ---------------------------------------------------------------------
+    case "XPR3,C030,HE-025A-QUE(2022-10-08)"
+        myTit="XPR3, C030mm, HE-025A-QUE scan (2022-10-08)";
+        monTypes=["CAM" "DDS"];
+        MonPaths=[...
+            "P:\Accelerating-System\Accelerator-data\Area dati MD\00XPR\XPR3\carbonio\Fuochi\Rough_scan\C_030mm\HE-025A-QUE\CarbSO2_*\"
+            "P:\Accelerating-System\Accelerator-data\Area dati MD\00XPR\XPR3\carbonio\Fuochi\Rough_scan\C_030mm\HE-025A-QUE\PRC-544-*\"
+            ];
+        myLabels=monTypes;
+        shifts=[0 0];
+    case "XPR3,C030,HE-023A-QUE(2022-10-08)"
+        myTit="XPR3, C030mm, HE-023A-QUE scan (2022-10-08)";
+        monTypes=["CAM" "DDS"];
+        MonPaths=[...
+            "P:\Accelerating-System\Accelerator-data\Area dati MD\00XPR\XPR3\carbonio\Fuochi\Rough_scan\C_030mm\HE-023A-QUE\CarbSO2_*\"
+            "P:\Accelerating-System\Accelerator-data\Area dati MD\00XPR\XPR3\carbonio\Fuochi\Rough_scan\C_030mm\HE-023A-QUE\PRC-544-*\"
+            ];
+        myLabels=monTypes;
+        shifts=[1 0];
+    case "XPR3,C030,HE-020A-QUE(2022-10-08)"
+        myTit="XPR3, C030mm, HE-020A-QUE scan (2022-10-08)";
+        monTypes=["CAM" "DDS"];
+        MonPaths=[...
+            "P:\Accelerating-System\Accelerator-data\Area dati MD\00XPR\XPR3\carbonio\Fuochi\Rough_scan\C_030mm\HE-020A-QUE\CarbSO2_*\"
+            "P:\Accelerating-System\Accelerator-data\Area dati MD\00XPR\XPR3\carbonio\Fuochi\Rough_scan\C_030mm\HE-020A-QUE\PRC-544-*\"
+            ];
+        myLabels=monTypes;
+        shifts=[1 0];
+    case "XPR3,C030,HE-018A-QUE(2022-10-08)"
+        myTit="XPR3, C030mm, HE-018A-QUE scan (2022-10-08)";
+        monTypes=["CAM" "DDS"];
+        MonPaths=[...
+            "P:\Accelerating-System\Accelerator-data\Area dati MD\00XPR\XPR3\carbonio\Fuochi\Rough_scan\C_030mm\HE-018A-QUE\CarbSO2_*\"
+            "P:\Accelerating-System\Accelerator-data\Area dati MD\00XPR\XPR3\carbonio\Fuochi\Rough_scan\C_030mm\HE-018A-QUE\PRC-544-*\"
+            ];
+        myLabels=monTypes;
+        shifts=[1 0];
+    case "XPR3,C030,H2-022A-QUE(2022-10-08)"
+        myTit="XPR3, C030mm, H2-022A-QUE scan (2022-10-08)";
+        monTypes=["CAM" "DDS"];
+        MonPaths=[...
+            "P:\Accelerating-System\Accelerator-data\Area dati MD\00XPR\XPR3\carbonio\Fuochi\Rough_scan\C_030mm\H2-022A-QUE\CarbSO2_*\"
+            "P:\Accelerating-System\Accelerator-data\Area dati MD\00XPR\XPR3\carbonio\Fuochi\Rough_scan\C_030mm\H2-022A-QUE\PRC-544-*\"
+            ];
+        myLabels=monTypes;
+        shifts=[1 0];
+
+    % =====================================================================
+	% %%%%%%%%%%%%%%%%% MISTERY of INTEGRAL %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % measurements 2023-02-26 (donetti's, without problem on integral)
+    % CAMeretta vs DDS
+    % =====================================================================
+
+    % ---------------------------------------------------------------------
+    % C270mm
+    % ---------------------------------------------------------------------
+    case "XPR3,C270,HE-025A-QUE,RUN1_CAMXPR(2023-02-26,DONETTI)"
+        myTit="XPR3, C270mm, HE-025A-QUE scan, Run1CamXPR (2023-02-26,Donetti)";
+        monTypes=["CAM" "DDS"];
+        MonPaths=[...
+            "S:\Area Ricerca\XPR\data\CAMeretta\2023-02-26_ProblemaIntegrale\Carbonio\HE-025A-QUE\270mm\Run1CamXPR\CarbSO2_*\"
+            "S:\Area Ricerca\XPR\data\CAMeretta\2023-02-26_ProblemaIntegrale\Carbonio\HE-025A-QUE\270mm\Run1CamXPR\PRC-544-*\"
+            ];
+        myLabels=monTypes;
+        shifts=[1 0];
+    case "XPR3,C270,HE-025A-QUE,RUN2_CAMXPR(2023-02-26,DONETTI)"
+        myTit="XPR3, C270mm, HE-025A-QUE scan, Run2CamXPR (2023-02-26,Donetti)";
+        monTypes=["CAM" "DDS"];
+        MonPaths=[...
+            "S:\Area Ricerca\XPR\data\CAMeretta\2023-02-26_ProblemaIntegrale\Carbonio\HE-025A-QUE\270mm\Run2CamXPR\CarbSO2_*\"
+            "S:\Area Ricerca\XPR\data\CAMeretta\2023-02-26_ProblemaIntegrale\Carbonio\HE-025A-QUE\270mm\Run2CamXPR\PRC-544-*\"
+            ];
+        myLabels=monTypes;
+        shifts=[1 0];
+    case "XPR3,C270,HE-025A-QUE,RUN3_CAMOLD_REV(2023-02-26,DONETTI)"
+        myTit="XPR3, C270mm, HE-025A-QUE scan, Run3CamOldRev (2023-02-26,Donetti)";
+        monTypes=["CAM" "DDS"];
+        MonPaths=[...
+            "S:\Area Ricerca\XPR\data\CAMeretta\2023-02-26_ProblemaIntegrale\Carbonio\HE-025A-QUE\270mm\Run3CamOldRev\CarbSO2_*\"
+            "S:\Area Ricerca\XPR\data\CAMeretta\2023-02-26_ProblemaIntegrale\Carbonio\HE-025A-QUE\270mm\Run3CamOldRev\PRC-544-*\"
+            ];
+        myLabels=monTypes;
+        shifts=[1 0];
+    case "XPR3,C270,HE-025A-QUE,RUN4_CAMOLD_REV(2023-02-26,DONETTI)"
+        myTit="XPR3, C270mm, HE-025A-QUE scan, Run4CamOldRev (2023-02-26,Donetti)";
+        monTypes=["CAM" "DDS"];
+        MonPaths=[...
+            "S:\Area Ricerca\XPR\data\CAMeretta\2023-02-26_ProblemaIntegrale\Carbonio\HE-025A-QUE\270mm\Run4CamOldRev\CarbSO2_*\"
+            "S:\Area Ricerca\XPR\data\CAMeretta\2023-02-26_ProblemaIntegrale\Carbonio\HE-025A-QUE\270mm\Run4CamOldRev\PRC-544-*\"
+            ];
+        myLabels=monTypes;
+        shifts=[1 0];
+    case "XPR3,C270,HE-025A-QUE,RUN5_CAMOLD(2023-02-26,DONETTI)"
+        myTit="XPR3, C270mm, HE-025A-QUE scan, Run5CamOld (2023-02-26,Donetti)";
+        monTypes=["CAM" "DDS"];
+        MonPaths=[...
+            "S:\Area Ricerca\XPR\data\CAMeretta\2023-02-26_ProblemaIntegrale\Carbonio\HE-025A-QUE\270mm\Run5CamOld\CarbSO2_*\"
+            "S:\Area Ricerca\XPR\data\CAMeretta\2023-02-26_ProblemaIntegrale\Carbonio\HE-025A-QUE\270mm\Run5CamOld\PRC-544-*\"
+            ];
+        myLabels=monTypes;
+        shifts=[1 0];
+    case "XPR3,C270,HE-025A-QUE,RUN6_CAMOLD(2023-02-26,DONETTI)"
+        myTit="XPR3, C270mm, HE-025A-QUE scan, Run6CamOld (2023-02-26,Donetti)";
+        monTypes=["CAM" "DDS"];
+        MonPaths=[...
+            "S:\Area Ricerca\XPR\data\CAMeretta\2023-02-26_ProblemaIntegrale\Carbonio\HE-025A-QUE\270mm\Run6CamOld\CarbSO2_*\"
+            "S:\Area Ricerca\XPR\data\CAMeretta\2023-02-26_ProblemaIntegrale\Carbonio\HE-025A-QUE\270mm\Run6CamOld\PRC-544-*\"
+            ];
+        myLabels=monTypes;
+        shifts=[1 0];
+    case "XPR3,C270,HE-025A-QUE,RUN7_CAM_NOHV(2023-02-26,DONETTI)"
+        myTit="XPR3, C270mm, HE-025A-QUE scan, Run7CamNoHV (2023-02-26,Donetti)";
+        monTypes=["CAM" "DDS"];
+        MonPaths=[...
+            "S:\Area Ricerca\XPR\data\CAMeretta\2023-02-26_ProblemaIntegrale\Carbonio\HE-025A-QUE\270mm\Run7CamNoHV\CarbSO2_*\"
+            "S:\Area Ricerca\XPR\data\CAMeretta\2023-02-26_ProblemaIntegrale\Carbonio\HE-025A-QUE\270mm\Run7CamNoHV\PRC-544-*\"
+            ];
+        myLabels=monTypes;
+        shifts=[1 0];
+
+    % =====================================================================
+	% %%%%%%%%%%%%%%%%% BEAM STEERING %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     % ISO2
     % =====================================================================
     
@@ -104,6 +357,7 @@ switch upper(loadSettings)
             ];
         
     % =====================================================================
+	% %%%%%%%%%%%%%%%%% BEAM STEERING %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     % ISO3
     % =====================================================================
     
