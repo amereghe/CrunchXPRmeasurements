f891b9eefbf0d2eb52f2fff7802b55a1727930f2
Mereghetti Alessio <Alessio.Mereghetti@Main.Cnao.group>
2023-02-28 13:07:09 +0100

    adding setups for checking SFP cabling

--- showData.m
+++ showData.m
@@ -15,36 +15,48 @@ addpath(genpath(pathToLibrary));
 clear loadSettings lSkip kPath;
 % please check chooseSettings.m
 
-% - amereghe's measurements
-mySettings="XPR3,C030,HE-025A-QUE(2022-10-08)";
-% myMagnets=["HE-025A-QUE" "HE-023A-QUE" "HE-020A-QUE" "HE-018A-QUE" ];
-% myBeamConfigs=["C270" "C150" "C030"];
-% mySettings=strings(length(myMagnets)*length(myBeamConfigs)+1,1);
-% kk=0;
-% for jj=1:length(myBeamConfigs)
-%     for ii=1:length(myMagnets)
-%         kk=kk+1;
-%         mySettings(kk)=sprintf("XPR3,%s,%s(2022-10-08)",myBeamConfigs(jj),myMagnets(ii));
-%     end
-% end
-% mySettings(end)="XPR3,C030,H2-022A-QUE(2022-10-08)";
+% -------------------------------------------------------------------------
+% CheckSFPcablaggi
+% -------------------------------------------------------------------------
 
-% - donettis's measurements
-mySettings="XPR3,C270,HE-025A-QUE,RUN2_CAMXPR(2023-02-26,DONETTI)";
-% mySettings=[
-%     "XPR3,C270,HE-025A-QUE,RUN3_CAMOLD_REV(2023-02-26,DONETTI)"
-%     "XPR3,C270,HE-025A-QUE,RUN4_CAMOLD_REV(2023-02-26,DONETTI)"
-%     "XPR3,C270,HE-025A-QUE,RUN5_CAMOLD(2023-02-26,DONETTI)"
-%     "XPR3,C270,HE-025A-QUE,RUN6_CAMOLD(2023-02-26,DONETTI)"
-%     "XPR3,C270,HE-025A-QUE,RUN7_CAM_NOHV(2023-02-26,DONETTI)"
-%     "XPR3,C270,HE-025A-QUE,RUN1_CAMXPR(2023-02-26,DONETTI)"
-%     "XPR3,C270,HE-025A-QUE,RUN2_CAMXPR(2023-02-26,DONETTI)"
-%     ];
+mySettings="HE-007A-CEB,HOR,PROT,320mm";
+% mySettings="HE-027A-CEB,HOR+VER,PROT,320mm";
 
-% - comparisons
-mySettings="XPR3,C270,HE-025A-QUE,SOLUTION(ALL)";
+% -------------------------------------------------------------------------
+% ProlemaIntegraleCameretta
+% -------------------------------------------------------------------------
+% % - amereghe's measurements
+% mySettings="XPR3,C030,HE-025A-QUE(2022-10-08)";
+% % myMagnets=["HE-025A-QUE" "HE-023A-QUE" "HE-020A-QUE" "HE-018A-QUE" ];
+% % myBeamConfigs=["C270" "C150" "C030"];
+% % mySettings=strings(length(myMagnets)*length(myBeamConfigs)+1,1);
+% % kk=0;
+% % for jj=1:length(myBeamConfigs)
+% %     for ii=1:length(myMagnets)
+% %         kk=kk+1;
+% %         mySettings(kk)=sprintf("XPR3,%s,%s(2022-10-08)",myBeamConfigs(jj),myMagnets(ii));
+% %     end
+% % end
+% % mySettings(end)="XPR3,C030,H2-022A-QUE(2022-10-08)";
+% 
+% % - donettis's measurements
+% mySettings="XPR3,C270,HE-025A-QUE,RUN2_CAMXPR(2023-02-26,DONETTI)";
+% % mySettings=[
+% %     "XPR3,C270,HE-025A-QUE,RUN3_CAMOLD_REV(2023-02-26,DONETTI)"
+% %     "XPR3,C270,HE-025A-QUE,RUN4_CAMOLD_REV(2023-02-26,DONETTI)"
+% %     "XPR3,C270,HE-025A-QUE,RUN5_CAMOLD(2023-02-26,DONETTI)"
+% %     "XPR3,C270,HE-025A-QUE,RUN6_CAMOLD(2023-02-26,DONETTI)"
+% %     "XPR3,C270,HE-025A-QUE,RUN7_CAM_NOHV(2023-02-26,DONETTI)"
+% %     "XPR3,C270,HE-025A-QUE,RUN1_CAMXPR(2023-02-26,DONETTI)"
+% %     "XPR3,C270,HE-025A-QUE,RUN2_CAMXPR(2023-02-26,DONETTI)"
+% %     ];
+% 
+% % - comparisons
+% mySettings="XPR3,C270,HE-025A-QUE,SOLUTION(ALL)";
 
-% - general settings
+% -------------------------------------------------------------------------
+% General settings
+% -------------------------------------------------------------------------
 lSkip=false; % DDS summary file: skip first 2 lines (in addition to header line)
 myFigPath=strcat(pwd,"\figs");
 
